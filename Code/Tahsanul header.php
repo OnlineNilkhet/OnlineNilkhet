ce2bf1aebcdc0e5400afa2bf291a3927bf3bbdd0
Tahsanul Karim <39657292+TahsanulKarim@users.noreply.github.com>
2018-08-04 01:09:13 +0600

    Add files via upload

---
+++ Code/Tahsanul header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>   
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<header>
+	<nav>
+		<div class="main-wrapper">
+			<ul>
+				<li><a href="index.php">Online Nilkhet</a></li>
+			</ul>
+			<div class="nav-signin">
+				<a href="signin.php">Sign In</a>
+			</div>
+	
+		</div>
+	</nav>
+</header>
