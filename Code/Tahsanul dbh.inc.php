a0f9e5978cb694bd6768834cd3fe1e70b89ad928
Tahsanul Karim <39657292+TahsanulKarim@users.noreply.github.com>
2018-08-04 01:17:33 +0600

    Add files via upload

---
+++ Code/Tahsanul dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "e-nilkhet";
+
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
