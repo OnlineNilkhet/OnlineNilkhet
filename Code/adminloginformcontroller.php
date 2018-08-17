5573520c8a0a44472292956efaf55dbbea389386
Rafiq <rafiqul.islam2.cse@ulab.edu.bd>
2018-08-17 06:04:39 +0600

    admin login page in controller

---
+++ Code/adminloginformcontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class adminloginformcontroller extends Controller
+{
+    function index(){
+
+    	return view('adminloginform');
+    }
+}
