5efe0dd9ed03ed0bd6ce9899607581d1855dc0c8
dilshadjahanrupaa <dilshadjahanrupa14@gmail.com>
2018-08-20 00:56:49 -0700

    login

--- Code/vendor_login.blade.php
+++ Code/vendor_login.blade.php
@@ -24,17 +24,6 @@
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;subset=latin,cyrillic-ext,latin-ext' rel='stylesheet' type='text/css'>
 	<!-- end: CSS -->
 	
-
-	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
-	<!--[if lt IE 9]>
-	  	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<link id="ie-style" href="css/ie.css" rel="stylesheet">
-	<![endif]-->
-	
-	<!--[if IE 9]>
-		<link id="ie9style" href="css/ie9.css" rel="stylesheet">
-	<![endif]-->
-		
 	<!-- start: Favicon -->
 	<link rel="shortcut icon" href="{{URL::to('backend/img/favicon.ico')}}">
 	<!-- end: Favicon -->
