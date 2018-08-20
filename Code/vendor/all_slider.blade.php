c108ad57059b8ff86ffc9c54ef56e67e6564baa1
dilshadjahanrupaa <dilshadjahanrupa14@gmail.com>
2018-08-20 00:51:41 -0700

    vendor

--- Code/vendor/all_slider.blade.php
+++ Code/vendor/all_slider.blade.php
@@ -22,60 +22,47 @@
 			<div class="row-fluid sortable">		
 				<div class="box span12">
 					<div class="box-header" data-original-title>
-						<h2><i class="halflings-icon user"></i><span class="break"></span>Members</h2>
+						<h2><i class="halflings-icon user"></i><span class="break"></span>Slider</h2>
 						
 					</div>
 					<div class="box-content">
 						<table class="table table-striped table-bordered bootstrap-datatable datatable">
 						  <thead>
 							  <tr>
-								  <th>Book ID</th>
-								  <th>Book Name</th>
-								 
-								  <th>Book Image</th>
-								  <th>Book Price</th>
-								  <th>Category Name</th>
-								  <th>Vendor Name</th>
+								  <th>Slider ID</th>
+								  <th>Slider Name</th>
 								  <th>Status</th>
 								  <th>Actions</th>
 							  </tr>
 						  </thead>
 
-						@foreach( $all_product_info as $v_product)
+						@foreach( $all_slider as $v_slider)
 						  <tbody>
 							<tr>
-								<td>{{ $v_product->book_id }}</td>
-								<td class="center">{{ $v_product->book_name }}</td>
+								<td>{{ $v_slider->slider_id }}</td>
+								<td><img src="{{URL::to($v_slider->slider_image)}}" style="height: 80px; width: 200px;"></td>
 								
-								<td><img src="{{URL::to($v_product->book_image)}}" style="height: 80px; width: 80px;"></td>
-								<td class="center">{{ $v_product->book_price }} Tk</td>
-								<td class="center">{{ $v_product->category_name }}</td>
-								<td class="center">{{ $v_product->vendor_name }}</td>
 								<td class="center">
 
-									@if( $v_product->publication_status == 1)
+									@if( $v_slider->publication_status == 1)
 									<span class="label label-success">Active</span>
 									@else
 									<span class="label label-danger">Unactive</span>
 									@endif
 								</td>
 								<td class="center">
-									@if( $v_product->publication_status == 1)
-									<a class="btn btn-danger" href="{{URL::to('/unactive-product/'. $v_product->book_id )}}">
+									@if( $v_slider->publication_status == 1)
+									<a class="btn btn-danger" href="{{URL::to('/unactive-slider/'. $v_slider->slider_id )}}">
 										<i class="halflings-icon white thumbs-down"></i>  
 									</a>
 
 									@else
-									<a class="btn btn-success" href="{{URL::to('/active-product/'. $v_product-> book_id )}}">
+									<a class="btn btn-success" href="{{URL::to('/active-slider/'. $v_slider-> slider_id )}}">
 										<i class="halflings-icon white thumbs-up"></i>  
 									</a>
 									@endif
 
-									
-									<a class="btn btn-info" href="{{URL::to('/edit-product/'. $v_product->book_id )}}">
-										<i class="halflings-icon white edit"></i>  
-									</a>
-									<a class="btn btn-danger" href="{{URL::to('/delete-product/'. $v_product->book_id )}}" id="delete">
+									<a class="btn btn-danger" href="{{URL::to('/delete-slider/'. $v_slider->slider_id )}}" id="delete">
 										<i class="halflings-icon white trash"></i> 
 									</a>
 								</td>
---
+++ Code/vendor/edit_product.blade.php
@@ -0,0 +1,52 @@
+@extends('vendor_layout')
+@section('vendor_content')
+
+<ul class="breadcrumb">
+				<li>
+					<i class="icon-home"></i>
+					<a href="index.html">Home</a>
+					<i class="icon-angle-right"></i> 
+				</li>
+				<li>
+					<i class="icon-edit"></i>
+					<a href="#">Update Category</a>
+				</li>
+			</ul>
+			
+			<div class="row-fluid sortable">
+				<div class="box span12">
+					<div class="box-header" data-original-title>
+						<h2><i class="halflings-icon edit"></i><span class="break"></span>Update Book</h2>
+					</div>
+
+						
+					<div class="box-content">
+						<form class="form-horizontal" action="{{url('/update-product',$product_info->book_id)}}" method="post">
+							{{ csrf_field() }}
+						  <fieldset>
+							<div class="control-group">
+							 
+							<div class="control-group">
+							  <label class="control-label" for="date01">Product Name</label>
+							  <div class="controls">
+								<input type="text" class="input-xlarge " name="book_name" value="{{$product_info->book_name}}">
+							  </div>
+							</div>
+
+
+							  <label class="control-label" for="date01">Product Price</label>
+							  <div class="controls">
+								<input type="text" class="input-xlarge " name="book_price" value="{{$product_info->book_price}}">
+							  </div>
+							<div class="form-actions">
+							  <button type="submit" class="btn btn-primary">Save</button>
+							 
+							</div>
+						  </fieldset>
+						</form>   
+
+					</div>
+				</div><!--/span-->
+			</div>
+
+@endsection
