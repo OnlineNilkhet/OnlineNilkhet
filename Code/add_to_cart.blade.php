70f4bb4db08301c0334426e141bcecc5e783e968
dilshadjahanrupaa <dilshadjahanrupa14@gmail.com>
2018-08-23 23:47:44 -0700

    add-to-cart

---
+++ Code/add_to_cart.blade.php
@@ -0,0 +1,142 @@
+@extends('layout')
+@section('content')
+
+<section id="cart_items">
+		<div class="container col-sm-12">
+			<div class="breadcrumbs">
+				<ol class="breadcrumb">
+				  <li><a href="#">Home</a></li>
+				  <li class="active">Shopping Cart</li>
+				</ol>
+			</div>
+			<div class="table-responsive cart_info">
+
+				<!--value ashche kina oita check korbe-->
+				<?php $contents=Cart::content(); ?> <!--value gula ase contents variable a-->
+
+				<table class="table table-condensed">
+					<thead>
+						<tr class="cart_menu">
+							<td class="image">Image</td>
+							
+							<td class="description">Name</td>
+							<td class="price">Price</td>
+							<td class="quantity">Quantity</td>
+							<td class="total">Total</td>
+							<td>Action</td>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach ($contents as $v_contents) {?>
+						<tr>
+							<td class="cart_product">
+								<a href=""><img src="{{URL::to($v_contents->options->image)}}" height="80px" width="50px" alt=""></a>
+							</td>
+							
+							<td class="cart_description">
+								 <h4><a href="">{{$v_contents->name}}</a></h4>
+							</td>
+							<td class="cart_price">
+								<p>{{$v_contents->price}}</p>
+							</td>
+							<td class="cart_quantity">
+								<div class="cart_quantity_button">
+									<a class="cart_quantity_up" href=""> + </a>
+									<input class="cart_quantity_input" type="text" name="quantity" value="{{$v_contents->qty}}" autocomplete="off" size="2">
+									<a class="cart_quantity_down" href=""> - </a>
+								</div>
+							</td>
+							<td class="cart_total">
+								<p class="cart_total_price">{{$v_contents->total}}</p>
+							</td>
+							<td class="cart_delete">
+								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+							</td>
+						</tr>
+							<?php }?>
+						
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</section> <!--/#cart_items-->
+<section id="do_action">
+		<div class="container">
+			<div class="heading">
+				<h3>What would you like to do next?</h3>
+				<p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
+			</div>
+			<div class="row">
+				<!--	<div class="col-sm-6">
+				
+					<div class="chose_area">
+						<ul class="user_option">
+							<li>
+								<input type="checkbox">
+								<label>Use Coupon Code</label>
+							</li>
+							<li>
+								<input type="checkbox">
+								<label>Use Gift Voucher</label>
+							</li>
+							<li>
+								<input type="checkbox">
+								<label>Estimate Shipping & Taxes</label>
+							</li>
+						</ul>
+						<ul class="user_info">
+							<li class="single_field">
+								<label>Country:</label>
+								<select>
+									<option>United States</option>
+									<option>Bangladesh</option>
+									<option>UK</option>
+									<option>India</option>
+									<option>Pakistan</option>
+									<option>Ucrane</option>
+									<option>Canada</option>
+									<option>Dubai</option>
+								</select>
+								
+							</li>
+							<li class="single_field">
+								<label>Region / State:</label>
+								<select>
+									<option>Select</option>
+									<option>Dhaka</option>
+									<option>London</option>
+									<option>Dillih</option>
+									<option>Lahore</option>
+									<option>Alaska</option>
+									<option>Canada</option>
+									<option>Dubai</option>
+								</select>
+							
+							</li>
+							<li class="single_field zip-field">
+								<label>Zip Code:</label>
+								<input type="text">
+							</li>
+						</ul>
+						<a class="btn btn-default update" href="">Get Quotes</a>
+						<a class="btn btn-default check_out" href="">Continue</a>
+					</div>
+				</div>-->
+				<div class="col-sm-8">
+					<div class="total_area">
+						<ul>
+							<li>Cart Sub Total <span>{{Cart::subtotal()}}</span></li>
+							<li>Eco Tax <span>{{Cart::tax()}}</span></li>
+							<li>Shipping Cost <span>Free</span></li>
+							<li>Total <span>{{Cart::total()}}</span></li>
+						</ul>
+							<a class="btn btn-default update" href="">Update</a>
+							<a class="btn btn-default check_out" href="">Check Out</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!--/#do_action-->
+
+
+@endsection
