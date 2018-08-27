21fece986dce56604ec1de61da9034ee6762e106
dilshadjahanrupaa <dilshadjahanrupa14@gmail.com>
2018-08-27 11:18:59 -0700

    layout

--- Code/layout.blade.php
+++ Code/layout.blade.php
@@ -88,9 +88,23 @@
                             <ul class="nav navbar-nav">
                                 <li><a href="#"><i class="fa fa-user"></i> Account</a></li>
                                 <li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
-                                <li><a href="{{URL::to('/login-check')}}"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+
+                                 <?php $customer_id=Session::get('customer_id'); ?>
+                                 <?php if($customer_id != Null) {?>
+                                <li><a href="{{URL::to('/checkout')}}"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+                            <?php } else{?>
+                                 <li><a href="{{URL::to('/login-check')}}"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+                             <?php } ?>
+
+                                
                                 <li><a href="{{URL::to('/show-cart')}}"><i class="fa fa-shopping-cart"></i> Cart</a></li>
-                                <li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>
+
+                                <?php $customer_id=Session::get('customer_id'); ?>
+                                <?php if($customer_id != Null) {?>
+                                <li><a href="{{URL::to('/customer-logout')}}"><i class="fa fa-lock"></i> Logout</a></li>
+                            <?php } else{?>
+                                 <li><a href="{{URL::to('/login-check')}}"><i class="fa fa-lock"></i> Login</a></li>
+                             <?php } ?>
                             </ul>
                         </div>
                     </div>
@@ -119,7 +133,7 @@
                                         <li><a href="product-details.html">Product Details</a></li> 
                                         <li><a href="{{URL::to('/login-check')}}">Checkout</a></li> 
                                         <li><a href="{{URL::to('/show-cart')}}">Cart</a></li> 
-                                        <li><a href="login.html">Login</a></li> 
+                                        
                                     </ul>
                                 </li> 
                                 <li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
