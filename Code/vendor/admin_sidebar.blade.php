5d7ef0cb938a1656d3548c8b273052d6b70d7d97
Md. Tashnov Faeem <40634111+faeem13t@users.noreply.github.com>
2018-09-03 02:21:44 +0600

    Add files via upload

---
+++ Code/vendor/admin_sidebar.blade.php
@@ -0,0 +1,18 @@
+<!--sidebar-menu-->
+<div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
+  <ul>
+    <li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
+    <li> <a href="charts.html"><i class="icon icon-th-list"></i> <span>Category</span></a> </li>
+    <li> <a href="widgets.html"><i class="icon icon-inbox"></i> <span>Add Product</span></a> </li>
+    <li><a href="tables.html"><i class="icon icon-th"></i> <span>Orders</span></a></li>
+ 
+    <li class="content"> <span>Monthly Sell</span>
+      <div class="progress progress-mini progress-danger active progress-striped">
+        <div style="width: 88%;" class=""></div>
+      </div>
+      <div class="stat">4500 Tk</div>
+    </li>
+    
+  </ul>
+</div>
+<!--sidebar-menu-->
