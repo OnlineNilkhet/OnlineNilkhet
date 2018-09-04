211a94bf9d89ca8a2e831fa155d34cbf4c01fdf7
dilshadjahanrupaa <dilshadjahanrupa14@gmail.com>
2018-09-04 12:01:53 -0700

    vendor

--- Code/vendor/dashboard.blade.php
+++ Code/vendor/dashboard.blade.php
@@ -1,54 +1,43 @@
-@extends('layouts.adminLayout.admin_design')
-@section('content')
+@extends('vendor_layout')
+@section('vendor_content')
+	
+			<ul class="breadcrumb">
+				<li>
+					<i class="icon-home"></i>
+					<a href="/dashboard">Home</a> 
+					<i class="icon-angle-right"></i>
+				</li>
+				<li><a href="/dashboard">Dashboard</a></li>
+			</ul>
 
-<!--main-container-part-->
-<div id="content">
-<!--breadcrumbs-->
-  <div id="content-header">
-    <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
-  </div>
-<!--End-breadcrumbs-->
+			<div class="row-fluid">
+				
+				<div class="span3 statbox purple" onTablet="span6" onDesktop="span3">
+					
+					<div class="number">854<i class="icon-arrow-up"></i></div>
+					<div class="title">Total Category</div>
+					<div class="footer">
+						<a href="#"> read full report</a>
+					</div>	
+				</div>
+				<div class="span3 statbox green" onTablet="span6" onDesktop="span3">
+					
+					<div class="number">123<i class="icon-arrow-up"></i></div>
+					<div class="title">Total Orders</div>
+					<div class="footer">
+						<a href="#"> read full report</a>
+					</div>
+				</div>
+				<div class="span3 statbox blue noMargin" onTablet="span6" onDesktop="span3">
+					
+					<div class="number">982<i class="icon-arrow-up"></i></div>
+					<div class="title">Total Products</div>
+					<div class="footer">
+						<a href="#"> read full report</a>
+					</div>
+				</div>
+				
+       
 
-<!--Action boxes-->
-  <div class="container-fluid">
-    <div class="quick-actions_homepage">
-      <ul class="quick-actions">
-        <li class="bg_lg span3"> <a href="charts.html"> <i class="icon icon-th-list"></i><span class="label label-success">7</span> Category
-        </a> </li>
-        <li class="bg_ly"> <a href="widgets.html"> <i class="icon-inbox"></i><span class="label label-success">101</span> Products </a> </li>
-        <li class="bg_lo"> <a href="tables.html"> <i class="icon-th"></i><span class="label label-success">16</span> Orders</a> </li>
-  
-
-      </ul>
-    </div>
-<!--End-Action boxes-->    
-
-<!--Chart-box-->    
-    <div class="row-fluid">
-      <div class="widget-box">
-        <div class="widget-title bg_lg"><span class="icon"><i class="icon-signal"></i></span>
-          <h5>Order Analytics</h5>
-        </div>
-        <div class="widget-content" >
-          <div class="row-fluid">
-            <div class="span9">
-              <div class="chart"></div>
-            </div>
-            <div class="span3">
-              <ul class="site-stats">
-                <li class="bg_lh"><i class="icon-tag"></i> <strong>42201</strong> <small>Total Orders</small></li>
-                <li class="bg_lh"><i class="icon-repeat"></i> <strong>70</strong> <small>Pending Orders</small></li>
-                <li class="bg_lh"><i class="icon-globe"></i> <strong>32</strong> <small>Online Orders</small></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-<!--End-Chart-box--> 
- 
-        
-
-<!--end-main-container-part-->
 
 @endsection
